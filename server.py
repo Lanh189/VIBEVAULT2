f0933a650e0fc3780d32bbe369206ef0aaf305f1
thuydung180 <dungttt21411@st.uel.edu.vn>
2024-07-28 21:18:25 +0700

    paypos chua xong

--- server.py
+++ server.py
@@ -2,18 +2,18 @@ import json
 import os
 import random
 
-from payos import PaymentData, ItemData, payos
+from payos import PaymentData, ItemData, PayOS
 from flask import Flask, render_template, jsonify, request
 
-payOS = PayOS(client_id= os.environ.get('PAYOS_CLIENT_ID'), api_key= os.environ.get('PAYOS_API_KEY'), checksum_key= os.environ.get('PYOS_CHECKSUM_KEY'))
+payOS = PayOS(client_id= os.environ.get('PAYOS_CLIENT_ID'), api_key= os.environ.get('PAYOS_API_KEY'), checksum_key= os.environ.get('PAYOS_CHECKSUM_KEY'))
 
-app = Flask(__name__, static_folder='public', static_url_path='', template_foler='public')
+app = Flask(__name__, static_folder='public', static_url_path='', template_folder='public')
 
-@app.rout('/create_payment_link', methods=['POST'])
+@app.route('/create_payment_link', methods=['POST'])
 def create_payment():
-    domain = "http://127.0.0.1:5500"
+    domain = "http://127.0.0.1:5000"
     try:
-        paymentData = PaymentData(orderCode=random.randint(1000, 99999), amount=1000, description="demo", cancelUrl= f"{domain}/cancel.html", returnUrl=f"{domain}/success.html")
+        paymentData = PaymentData(orderCode=random.randint(1000, 99999), amount=1000, description="demo", cancelUrl= f"{domain}/cancel.html", returnUrl=f"{domain}/download.html")
         payOSCreatePayment = payOS.createPaymentLink(paymentData)
         return jsonify(payOSCreatePayment.to_json())
     except Exception as e:
