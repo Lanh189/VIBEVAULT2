21a50ae75b7fd8b27bb4d554dd671e34b54ccff2
thuydung180 <dungttt21411@st.uel.edu.vn>
2024-07-28 23:50:23 +0700

    unchange

--- server.py
+++ server.py
@@ -7,17 +7,18 @@ from flask import Flask, render_template, jsonify, request
 
 payOS = PayOS(client_id= os.environ.get('PAYOS_CLIENT_ID'), api_key= os.environ.get('PAYOS_API_KEY'), checksum_key= os.environ.get('PAYOS_CHECKSUM_KEY'))
 
-app = Flask(__name__, static_folder='public', static_url_path='', template_folder='public')
+app = Flask(__name__, static_folder='', static_url_path='', template_folder='')
 
 @app.route('/create_payment_link', methods=['POST'])
 def create_payment():
     domain = "http://127.0.0.1:5000"
     try:
-        paymentData = PaymentData(orderCode=random.randint(1000, 99999), amount=2000, description="demo", cancelUrl= f"{domain}/cancel.html", returnUrl=f"{domain}/download.html")
+        paymentData = PaymentData(orderCode=random.randint(1000, 99999), amount=1000, description="demo", cancelUrl= f"{domain}/cancel.html", returnUrl=f"{domain}/download.html")
         payOSCreatePayment = payOS.createPaymentLink(paymentData)
-        return jsonify(payOSCreatePayment.to_json())
+        return jsonify({'payment_link': payOSCreatePayment.paymentUrl})
     except Exception as e:
-        return jsonify(error=str(e)), 403
+        return jsonify({'error': str(e)}), 403
+
     
     if __name__ == '__main__':
         app.run(port=4242)
